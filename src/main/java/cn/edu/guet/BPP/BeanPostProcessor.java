6029110411556115bb9ee552c922b46954350e97
theChenSir <102413276+theChenSir@users.noreply.github.com>
2022-08-18 12:24:41 +0800

    Delete src/main/java/cn/edu/guet directory

--- src/main/java/cn/edu/guet/BPP/BeanPostProcessor.java
+++
@@ -1,8 +0,0 @@
-package cn.edu.guet.BPP;
-
-import org.springframework.beans.BeansException;
-
-public interface BeanPostProcessor {
-    Object postProcessBeforeInitialization(Object bean, String beanName) ;
-    Object postProcessAfterInitialization(Object bean, String beanName) ;
-}
--- src/main/java/cn/edu/guet/mapper/IPermissionDao.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.guet.mapper;
-
-import cn.edu.guet.bean.Permission;
-
-import java.util.List;
-
-public interface IPermissionDao {
-    List<Permission> permissionList();
-}
